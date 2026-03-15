c5bf13eefd754b0d3e87cc995216b3be356893c0
julcalde <julcalde@student.42heilbronn.de>
2026-03-15 14:57:07 +0100

    [ex00/main.cpp]program checks for arguments, creates an instance of BitcoinExchange, tries loading data.csv into map container and finally processes, computes and outputs results from argument against database.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,15 +6,26 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 15:28:22 by julcalde          #+#    #+#             */
-/*   Updated: 2026/01/11 16:53:29 by julcalde         ###   ########.fr       */
+/*   Updated: 2026/03/15 14:21:35 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 #include <iostream>
 
-int main(void)
+int main(int argc, char **argv)
 {
-
+	if (argc != 2)
+	{
+		std::cerr << "Error: could not open file." << std::endl;
+		return (1);
+	}
+	BitcoinExchange exchanger;
+	if (!exchanger.loadDatabase("data.csv"))
+	{
+		std::cerr << "Error: could not load database." << std::endl;
+		return (1);
+	}
+	exchanger.processInput(argv[1]);
 	return (0);
 }
