6005ed3220599bc474ef05c3c757bfa0598e514e
julcalde <julcalde@student.42heilbronn.de>
2026-03-15 16:00:58 +0100

    [ex01/main.cpp]the program checks for enough arguments, then 'evaluates' the expression given as second argument to either throw an error or calculate try solving the expression.

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,9 +6,26 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/11 16:52:59 by julcalde          #+#    #+#             */
-/*   Updated: 2026/01/11 16:56:57 by julcalde         ###   ########.fr       */
+/*   Updated: 2026/03/15 15:19:26 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
+#include <iostream>
 
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		std::cerr << " Error: usage: " << argv[0] << " \"expression\"" << std::endl;
+		return (1);
+	}
+	int result;
+	if (!RPN::evaluate(argv[1], result))
+	{
+		std::cerr << "Error: invalid expression." << std::endl;
+		return (1);
+	}
+	std::cout << result << std::endl;
+	return (0);
+}
