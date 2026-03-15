309b8efb287e1fb5c12f92e0606fa4e387725801
julcalde <julcalde@student.42heilbronn.de>
2026-03-15 14:49:06 +0100

    [ex00/BitcoinExchange.hpp]ignore

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 15:28:24 by julcalde          #+#    #+#             */
-/*   Updated: 2026/01/11 17:50:32 by julcalde         ###   ########.fr       */
+/*   Updated: 2026/03/15 13:00:35 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ class BitcoinExchange
 		BitcoinExchange& operator=(const BitcoinExchange&);
 
 		bool loadDatabase(const std::string& filename);
-		void processInput(const std::string& filename);
+		void processInput(const std::string& filename) const;
 };
 
 #endif
